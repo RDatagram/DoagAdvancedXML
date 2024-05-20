0b4c2af770157e68f19a0db8746b76a2502a5c93
RDatagram <zoran.r@rdata.rs>
2024-05-20 15:53:08 +0200

    Added workflow action script for invoice XML rendering
    Implemented a new workflow action script for invoice XML rendering. With this script, invoices can now be generated in XML format using a JSON data source. The script specifies item IDs, quantities, and amounts in the invoice. A new XML template for invoices was also added.

---
+++ src/FileCabinet/SuiteScripts/AdvancedXML/src/demo_wfa_invoice_xml.js
@@ -0,0 +1,85 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType WorkflowActionScript
+ */
+define(['N/render', 'N/file'],
+    /**
+ * @param{render} render
+ */
+    (render, file) => {
+        /**
+         * Defines the WorkflowAction script trigger point.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.workflowId - Internal ID of workflow which triggered this action
+         * @param {string} scriptContext.type - Event type
+         * @param {Form} scriptContext.form - Current form that the script uses to interact with the record
+         * @since 2016.1
+         */
+        const onAction = (scriptContext) => {
+            const xmlTemplateFile = file.load({
+                id : '../templates/word.template.xml'
+            });
+            const  xmlTemplate = xmlTemplateFile.getContents();
+            const renderer = render.create();
+            renderer.templateContent = xmlTemplate;
+
+            const numberOfLines = scriptContext.newRecord.getLineCount({
+                sublistId: 'item'
+            });
+
+            let jsonData = {};
+            jsonData.items = [];
+
+            for (let i = 0; i < numberOfLines; i++) {
+                const itemId = scriptContext.newRecord.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'item',
+                    line: i
+                });
+
+                const quantity = scriptContext.newRecord.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'quantity',
+                    line: i
+                });
+
+                const amount = scriptContext.newRecord.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'amount',
+                    line: i
+                });
+
+                jsonData.items.push(
+                    {
+                        "item" : itemId,
+                        "quantity" : quantity,
+                        "amount" : amount
+                    }
+                )
+                log.debug('Item: ' + itemId + ', Quantity: ' + quantity);
+            }
+
+
+            renderer.addCustomDataSource({
+                format: render.DataSource.JSON,
+                alias: "JSON",
+                data: JSON.stringify(jsonData)
+            });
+
+            // render as string
+            const renderedContent = renderer.renderAsString();
+            const xlsFile = file.create({
+                name: 'invoice.doc',
+                fileType: file.Type.XMLDOC,
+                contents: renderedContent
+            });
+            log.debug('Rendered Content:', renderedContent);
+            xlsFile.folder = 417;
+            xlsFile.save();
+
+        }
+
+        return {onAction};
+    });
